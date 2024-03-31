481df9e087709fc238fbac3776301c0bd47a1210
nguyenhoangtuan12102003 <116452243+nguyenhoangtuan12102003@users.noreply.github.com>
2024-03-31 19:35:53 +0700

    update

--- src/main/java/GUI/HomeGUI.java
+++ src/main/java/GUI/HomeGUI.java
@@ -4,6 +4,15 @@
  */
 package GUI;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author ASUS
@@ -11,10 +20,42 @@ package GUI;
 public class HomeGUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form HomeGUI
+     * Creates new form HomeGUI2
      */
     public HomeGUI() {
         initComponents();
+        setLocationRelativeTo(null);
+        panelShow.setLayout(new CardLayout());
+        // panel course
+        JPanel pnTV = new JPanel();
+        PaneThanhVien thanhVien = new PaneThanhVien();
+        pnTV.setLayout(new BorderLayout());
+        pnTV.add(thanhVien, BorderLayout.CENTER);
+
+        //panel phan cong
+        JPanel pnThietBi = new JPanel();
+        PaneThietBi thietBi = new PaneThietBi();
+        pnThietBi.setLayout(new BorderLayout());
+        pnThietBi.add(thietBi, BorderLayout.CENTER);
+
+        //panel ket qua
+        JPanel pnXuLy = new JPanel();
+        PanelXuLy xuLy = new PanelXuLy();
+        pnXuLy.setLayout(new BorderLayout());
+        pnXuLy.add(xuLy, BorderLayout.CENTER);
+
+        //panel thong ke
+        JPanel pnThongKe = new JPanel();
+        PanelThongKe thongKe = new PanelThongKe();
+        pnThongKe.setLayout(new BorderLayout());
+        pnThongKe.add(thongKe, BorderLayout.CENTER);
+
+        //
+        panelShow.add(pnTV, "thanhVien");
+        panelShow.add(pnThietBi, "thietBi");
+        panelShow.add(pnXuLy, "xuLy");
+        panelShow.add(pnThongKe, "thongKe");
+
     }
 
     /**
@@ -26,21 +67,214 @@ public class HomeGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel4 = new javax.swing.JPanel();
+        jToggleButton1 = new javax.swing.JToggleButton();
+        jPanel6 = new javax.swing.JPanel();
+        pnMenu = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        btnThietBi = new javax.swing.JButton();
+        btnThanhVien = new javax.swing.JButton();
+        btnXuLy = new javax.swing.JButton();
+        btnThongKe = new javax.swing.JButton();
+        panelShow = new javax.swing.JPanel();
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        jToggleButton1.setText("jToggleButton1");
+
+        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
+        jPanel6.setLayout(jPanel6Layout);
+        jPanel6Layout.setHorizontalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel6Layout.setVerticalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Management Course");
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setLocation(new java.awt.Point(0, 0));
+
+        pnMenu.setBackground(new java.awt.Color(255, 255, 255));
+        pnMenu.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 102, 51));
+        jLabel1.setText("MANAGEMENT MEMBERS");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(56, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(53, 53, 53))
+        );
+
+        btnThietBi.setBackground(new java.awt.Color(204, 204, 204));
+        btnThietBi.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnThietBi.setText("THiẾT BỊ");
+        btnThietBi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnThietBiActionPerformed(evt);
+            }
+        });
+
+        btnThanhVien.setBackground(new java.awt.Color(204, 204, 204));
+        btnThanhVien.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnThanhVien.setText("THÀNH VIÊN");
+        btnThanhVien.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnThanhVienActionPerformed(evt);
+            }
+        });
+
+        btnXuLy.setBackground(new java.awt.Color(204, 204, 204));
+        btnXuLy.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnXuLy.setText("XỬ LÝ");
+        btnXuLy.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnXuLyActionPerformed(evt);
+            }
+        });
+
+        btnThongKe.setBackground(new java.awt.Color(204, 204, 204));
+        btnThongKe.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnThongKe.setText("THỐNG KÊ");
+        btnThongKe.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnThongKeActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout pnMenuLayout = new javax.swing.GroupLayout(pnMenu);
+        pnMenu.setLayout(pnMenuLayout);
+        pnMenuLayout.setHorizontalGroup(
+            pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnMenuLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnThongKe, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnThanhVien, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnThietBi, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 211, Short.MAX_VALUE)
+                    .addComponent(btnXuLy, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        pnMenuLayout.setVerticalGroup(
+            pnMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnMenuLayout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addComponent(btnThanhVien, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnThietBi, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnXuLy, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnThongKe, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout panelShowLayout = new javax.swing.GroupLayout(panelShow);
+        panelShow.setLayout(panelShowLayout);
+        panelShowLayout.setHorizontalGroup(
+            panelShowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1026, Short.MAX_VALUE)
+        );
+        panelShowLayout.setVerticalGroup(
+            panelShowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 601, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(pnMenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelShow, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(pnMenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelShow, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnThanhVienActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThanhVienActionPerformed
+        CardLayout cardLayout = (CardLayout) panelShow.getLayout();
+        cardLayout.show(panelShow, "thanhVien");
+        btnThanhVien.setBackground(Color.red);
+        btnThietBi.setBackground(Color.white);
+        btnXuLy.setBackground(Color.white);
+        btnThongKe.setBackground(Color.white);
+
+
+    }//GEN-LAST:event_btnThanhVienActionPerformed
+
+    private void btnThietBiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThietBiActionPerformed
+        CardLayout cardLayout = (CardLayout) panelShow.getLayout();
+        cardLayout.show(panelShow, "thietBi");
+        btnThanhVien.setBackground(Color.white);
+        btnThietBi.setBackground(Color.red);
+        btnXuLy.setBackground(Color.white);
+        btnThongKe.setBackground(Color.white);
+
+
+    }//GEN-LAST:event_btnThietBiActionPerformed
+
+    private void btnXuLyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnXuLyActionPerformed
+        CardLayout cardLayout = (CardLayout) panelShow.getLayout();
+        cardLayout.show(panelShow, "xuLy");
+        btnThanhVien.setBackground(Color.white);
+        btnThietBi.setBackground(Color.white);
+        btnXuLy.setBackground(Color.red);
+        btnThongKe.setBackground(Color.white);
+
+    }//GEN-LAST:event_btnXuLyActionPerformed
+
+    private void btnThongKeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThongKeActionPerformed
+        CardLayout cardLayout = (CardLayout) panelShow.getLayout();
+        cardLayout.show(panelShow, "thongKe");
+        btnThanhVien.setBackground(Color.white);
+        btnThietBi.setBackground(Color.white);
+        btnXuLy.setBackground(Color.white);
+        btnThongKe.setBackground(Color.red);
+
+    }//GEN-LAST:event_btnThongKeActionPerformed
 
     /**
      * @param args the command line arguments
@@ -68,15 +302,42 @@ public class HomeGUI extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(HomeGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+                try {
+                    UIManager.setLookAndFeel(
+                            UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(HomeGUI.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InstantiationException ex) {
+                    Logger.getLogger(HomeGUI.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(HomeGUI.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (UnsupportedLookAndFeelException ex) {
+                    Logger.getLogger(HomeGUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
                 new HomeGUI().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnThanhVien;
+    private javax.swing.JButton btnThietBi;
+    private javax.swing.JButton btnThongKe;
+    private javax.swing.JButton btnXuLy;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel6;
+    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JPanel panelShow;
+    private javax.swing.JPanel pnMenu;
     // End of variables declaration//GEN-END:variables
 }
