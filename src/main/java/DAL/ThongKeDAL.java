61dc8e0afebf1a49b66c27e1884ccce92babd495
nguyenhoangtuan12102003 <116452243+nguyenhoangtuan12102003@users.noreply.github.com>
2024-04-14 16:08:55 +0700

    update

--- src/main/java/DAL/ThongKeDAL.java
+++ src/main/java/DAL/ThongKeDAL.java
@@ -5,13 +5,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ThongKeDAL {
-    public static List<thanhvien> getDataThanhVien(){
-        List<thanhvien> thanhvien  = new ArrayList<>();
-        try {
-            
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-    }
+ 
 }
